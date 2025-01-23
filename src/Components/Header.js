c5daa27fbe2549e7babfcfb43abdb91c2b1bbf43
Mo Yameen Macchhar <85780420+yameen7@users.noreply.github.com>
2025-01-23 10:35:46 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <button className=" btn navbar-brand">Todos List</button>
+                <button className=" btn navbar-brand">My Todos List</button>
             </div>
         </nav>
     )
